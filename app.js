325ab1cb1c6db51e812f07526ebfe9320dadd4b8
Brian Tollett <161470130+33bree3@users.noreply.github.com>
2025-07-22 18:55:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -53,14 +53,14 @@ scene.add(sun);
 
 // Array holding planet data and properties
 const planets = [
-  { name: 'Mercury', data: new Planet(mercuryData), color: 0xc0c0c0, radius: 30 },
-  { name: 'Venus',   data: new Planet(venusData),   color: 0xf5deb3, radius: 39 },
-  { name: 'Earth',   data: new Planet(earthData),   color: 0x1e90ff, radius: 42 },
-  { name: 'Mars',    data: new Planet(marsData),    color: 0xff4500, radius: 54 },
-  { name: 'Jupiter', data: new Planet(jupiterData), color: 0xf4e2d8, radius: 66 },
-  { name: 'Saturn',  data: new Planet(saturnData),  color: 0xdeb887, radius: 72 },
-  { name: 'Uranus',  data: new Planet(uranusData),  color: 0x7fffd4, radius: 81 },
-  { name: 'Neptune', data: new Planet(neptuneData), color: 0x4169e1, radius: 102 },
+  { name: 'Mercury', data: new Planet(mercuryData), color: 0xc0c0c0, radius:  3 },
+  { name: 'Venus',   data: new Planet(venusData),   color: 0xf5deb3, radius:  6 },
+  { name: 'Earth',   data: new Planet(earthData),   color: 0x1e90ff, radius:  9 },
+  { name: 'Mars',    data: new Planet(marsData),    color: 0xff4500, radius: 12 },
+  { name: 'Jupiter', data: new Planet(jupiterData), color: 0xf4e2d8, radius: 15 },
+  { name: 'Saturn',  data: new Planet(saturnData),  color: 0xdeb887, radius: 18 },
+  { name: 'Uranus',  data: new Planet(uranusData),  color: 0x7fffd4, radius: 21 },
+  { name: 'Neptune', data: new Planet(neptuneData), color: 0x4169e1, radius: 24 },
 ];
 
 // Create a Three.js Mesh for each planet and add to the scene
