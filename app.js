abd1299b36f4ab367f1ec832981370cf2224989e
Brian Tollett <161470130+33bree3@users.noreply.github.com>
2025-07-22 19:15:31 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // app.js
+
 import * as THREE from 'https://unpkg.com/three@0.158.0/build/three.module.js';
 import { OrbitControls } from 'https://unpkg.com/three@0.158.0/examples/jsm/controls/OrbitControls.js';
 
