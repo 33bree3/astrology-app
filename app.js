c873811f49a64c51ca1a43fa9ea30e044724097a
Brian Tollett <161470130+33bree3@users.noreply.github.com>
2025-07-23 14:50:48 +0100

    Update app.js

--- app.js
+++ app.js
@@ -176,8 +176,8 @@ function animate() {
   sunLight.position.copy(solarSystem.position);
 
   // Parameters for elliptical orbits
-  const orbitA = 100; // semi-major axis (X)
-  const orbitB = 100; // semi-minor axis (Y)
+  const orbitA = 9999; // semi-major axis (X)
+  const orbitB = 999; // semi-minor axis (Y)
   const orbitSpeed = 0.05;
   const helixZSpacing = -111;
 
