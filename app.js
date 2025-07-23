3b7983190fed7ba1003d1cf3fc37c04f5b176e62
Brian Tollett <161470130+33bree3@users.noreply.github.com>
2025-07-23 18:01:50 +0100

    Update app.js

--- app.js
+++ app.js
@@ -118,13 +118,13 @@ const planetTextures = {
 const scaleFactor = 0.1;
 const planets = [
   { name: 'Mercury', data: new Planet(mercuryData), radius: 111, planetSize: 12 },
-  { name: 'Venus',   data: new Planet(venusData),   radius: 333, planetSize: 18 },
-  { name: 'Earth',   data: new Planet(earthData),   radius: 666, planetSize: 21 },
-  { name: 'Mars',    data: new Planet(marsData),    radius: 777, planetSize: 15 },
-  { name: 'Jupiter', data: new Planet(jupiterData), radius: 1010, planetSize: 36 },
-  { name: 'Saturn',  data: new Planet(saturnData),  radius: 1212, planetSize: 30 },
-  { name: 'Uranus',  data: new Planet(uranusData),  radius: 1414, planetSize: 24 },
-  { name: 'Neptune', data: new Planet(neptuneData), radius: 1717, planetSize: 24 },
+  { name: 'Venus',   data: new Planet(venusData),   radius: 222, planetSize: 18 },
+  { name: 'Earth',   data: new Planet(earthData),   radius: 444, planetSize: 21 },
+  { name: 'Mars',    data: new Planet(marsData),    radius: 555, planetSize: 15 },
+  { name: 'Jupiter', data: new Planet(jupiterData), radius: 777, planetSize: 36 },
+  { name: 'Saturn',  data: new Planet(saturnData),  radius: 888, planetSize: 30 },
+  { name: 'Uranus',  data: new Planet(uranusData),  radius: 999, planetSize: 24 },
+  { name: 'Neptune', data: new Planet(neptuneData), radius: 1010, planetSize: 24 },
 ];
 
 // Create planet mesh objects with material
