11324bb745ef1215d23d264206658895f0dc23a3
Brian Tollett <161470130+33bree3@users.noreply.github.com>
2025-07-24 19:27:44 +0100

    Update app.js

--- app.js
+++ app.js
@@ -282,8 +282,8 @@ function animate() {
   const jd = julian.DateToJD(new Date());
 
   
-  const scale = 3333; // scale for visibility
-  const baseScale = 3333; // 🌟 MOVE IT HERE
+  const scale = 333; // scale for visibility
+  const baseScale = 333; // 🌟 MOVE IT HERE
 
   solarSystem.position.set(0, 0, 0);
   sunLight.position.copy(sun.position);
