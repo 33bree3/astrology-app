7e79962b37e554a296ccdcc737626836b22bc5f7
Brian Tollett <161470130+33bree3@users.noreply.github.com>
2025-07-24 14:43:54 +0100

    Update app.js

--- app.js
+++ app.js
@@ -210,7 +210,7 @@ const moonMesh = new THREE.Mesh(
   })
 );
 
-moonMesh scale.set(1, 1, 1);  // Adjust 0.5r size factor you want
+moonMesh.scale.set(1, 1, 1);  // Adjust 0.5r size factor you want
 moonMesh.castShadow = true;
 moonMesh.receiveShadow = true;
 scene.add(moonMesh);
