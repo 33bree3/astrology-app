974d8226bafcaad3c3b984b2ffb3fa9627012fc1
Brian Tollett <161470130+33bree3@users.noreply.github.com>
2025-07-26 11:15:15 +0100

    Update app.js

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ const PLANET_SIZE_MULTIPLIER = 1;
 const TIME_SPEED_FACTOR = 5;
 const degToRad = deg => deg * Math.PI / 180;
 
-// Orbital elements for each planet
+// Orbital elements for each planet a = orbit size , e - eccentritiy 
 const orbitalElementsData = {
   Mercury: { a: 0.5555, e: 0, i: degToRad(7.005),   o: degToRad(48.331),  w: degToRad(29.124) },
   Venus:   { a: 0.7777, e: 0, i: degToRad(3.3946),  o: degToRad(76.680),  w: degToRad(54.884) },
-  Earth:   { a: 0.9999, e: 0.01, i: degToRad(0.000),   o: degToRad(0.000),   w: degToRad(114.207) },
-  Mars:    { a: 1.2222, e: 0.03, i: degToRad(1.850),   o: degToRad(49.558),  w: degToRad(286.502) },
+  Earth:   { a: 1.2222, e: 0.01, i: degToRad(0.000),   o: degToRad(0.000),   w: degToRad(114.207) },
+  Mars:    { a: 1.6666, e: 0.03, i: degToRad(1.850),   o: degToRad(49.558),  w: degToRad(286.502) },
   Jupiter: { a: 2.0000, e: 0.04, i: degToRad(1.303),   o: degToRad(100.464), w: degToRad(273.867) },
   Saturn:  { a: 2.5555, e: 0.04,  i: degToRad(2.489),   o: degToRad(113.665), w: degToRad(339.392) },
   Uranus:  { a: 3.0000, e: 0.02, i: degToRad(0.773),   o: degToRad(74.006),  w: degToRad(96.998) },
