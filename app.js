33448807989357cfdb24e5d87baa7535639023a3
Brian Tollett <161470130+33bree3@users.noreply.github.com>
2025-10-31 13:31:46 +0000

    last update 31/oct/2025
    Home page - 
    
    solar system in place / some planets need better textures / planet & system sizing needs work / 
    
    todays chart should be placed on home page (chart page should be a user "personal" chart only)
    zodiac signs in the background need to be put in place for visuals 
    
    Chart page - 
    
    working as intended real time data for -> 
    | planet | geocentric longtitude | zodiac position| 
    w date input variation 
    
    (: visual work needed :)
    
    solar data page - 
    
    no work done

--- app.js
+++ app.js
@@ -216,7 +216,7 @@ const textureLoader = new THREE.TextureLoader();
 
 
 
-const BASE_SCALE = 88888, PLANET_SIZE_MULTIPLIER = 12 , TIME_SPEED_FACTOR = 0.1, radius = 100000000;
+const BASE_SCALE = 22222, PLANET_SIZE_MULTIPLIER = 12 , TIME_SPEED_FACTOR = 0.1, radius = 10000000;
 const degToRad = deg => deg * Math.PI / 180;
 
 
@@ -294,7 +294,7 @@ scene.add(new THREE.AmbientLight(0xffffff, 0.3));
 
 
 
-const sunGeometry = new THREE.SphereGeometry(3333, 333, 333);
+const sunGeometry = new THREE.SphereGeometry(1111, 333, 333);
 const sunMaterial = new THREE.MeshStandardMaterial({ map: planetTextures.Sun, emissive: 0xffff00, emissiveIntensity: 1 });
 scene.add(new THREE.Mesh(sunGeometry, sunMaterial));
 
