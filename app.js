e724b09e44a83b77d4a2d3df63157bc2e66bd4cd
Brian Tollett <161470130+33bree3@users.noreply.github.com>
2025-10-31 12:48:43 +0000

    Update BASE_SCALE and PLANET_SIZE_MULTIPLIER values

--- app.js
+++ app.js
@@ -200,7 +200,7 @@ const textureLoader = new THREE.TextureLoader();
 
 // ------------------------------------------------------  Constants
 
-const BASE_SCALE = 888, PLANET_SIZE_MULTIPLIER = 9 , TIME_SPEED_FACTOR = 1, radius = 100000;
+const BASE_SCALE = 8888, PLANET_SIZE_MULTIPLIER = 1 , TIME_SPEED_FACTOR = 1, radius = 100000;
 const degToRad = deg => deg * Math.PI / 180;
 
 
