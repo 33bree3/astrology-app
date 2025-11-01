d84d7afd7c5815450ae2f150fdd18cbc078774fb
Brian Tollett <161470130+33bree3@users.noreply.github.com>
2025-11-01 12:53:16 +0000

    Update app.js

--- app.js
+++ app.js
@@ -230,7 +230,7 @@ const textureLoader = new THREE.TextureLoader();
 
 
 
-const BASE_SCALE = 1111, PLANET_SIZE_MULTIPLIER = 1 , TIME_SPEED_FACTOR = 1, radius = 8888888888;
+const BASE_SCALE = 11111, PLANET_SIZE_MULTIPLIER = 1 , TIME_SPEED_FACTOR = 1, radius = 888888888;
 const degToRad = deg => deg * Math.PI / 180;
 
 
@@ -251,7 +251,7 @@ const orbitalElementsData = {
 };
 
 // helper 
-const BASE_SCALE = 40;
+
 const toRad = deg => (deg * Math.PI) / 180;
 
 
