b267dae2d34201df9747d419b679e66a8872632e
Brian Tollett <161470130+33bree3@users.noreply.github.com>
2025-11-01 12:03:17 +0000

    Update app.js

--- app.js
+++ app.js
@@ -216,7 +216,7 @@ const textureLoader = new THREE.TextureLoader();
 
 
 
-const BASE_SCALE = 444, PLANET_SIZE_MULTIPLIER = 1 , TIME_SPEED_FACTOR = 1, radius = 99990000000000;
+const BASE_SCALE = 444, PLANET_SIZE_MULTIPLIER = 1 , TIME_SPEED_FACTOR = 100, radius = 9;
 const degToRad = deg => deg * Math.PI / 180;
 
 
